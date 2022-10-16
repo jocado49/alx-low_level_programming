27da5fdd213d278bf535acd244cb1eee5749dfbf
Aralu <aralujoshua@gmail.com>
2022-10-16 15:59:18 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main-Entry
- * Return Always 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
-
 {
 	int n;
 	int a;
