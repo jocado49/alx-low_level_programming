9f6b13ab0a957c8e08b9437797b90553918c66b0
Aralu <aralujoshua@gmail.com>
2022-10-16 18:31:21 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all strings from 0 to 9
+ * Return: Always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; nu++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
