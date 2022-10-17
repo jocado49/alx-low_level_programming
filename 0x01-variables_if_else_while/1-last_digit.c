bfc709bb12169b83e05adb62c7e69cc8ef52f11c
Aralu <aralujoshua@gmail.com>
2022-10-17 15:06:16 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,35 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - entry point
+ * Return: Always return 0
  */
+/*checks if last digit of modulo is 5, less than 6 or equal to 0*/
 int main(void)
 {
 	int n;
 	int a;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	a = n % 10;
-	
-	if (a > 5)
+	lastn = n % 10;
+	if (lastn > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n", n, a);
+		printf("last digit of %d is %d and is greater than 5\n", n, lastn
 	}
-	else if (a == 0)
+
+	else if (lastn == 0)
+
 	{
-		printf("last digit of %d is %d and is 0\n", n, a);
+		printf("last digit of %d is %d and is 0\n", n, lastn)
 	}
+
 	else
+
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, a);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
 	}
 	return (0);
 }
