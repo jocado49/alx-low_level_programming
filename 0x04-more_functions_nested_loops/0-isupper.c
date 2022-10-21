4cee9a0ef0bca1e3b116f86c777347464f5e523a
Aralu <aralujoshua@gmail.com>
2022-10-21 15:09:01 -0700

    task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
+ * _isupper - checks for an uppercase character.
+ * @c: the character to be checked
+ *
  * Return: 1 if it is an uppercase character, 0 in other case
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <=90)
+	if (c >= 'A' && c <= 'Z')
 	{
-		return (1)
+	return (1);
 	}
 	else
 	{
-		return (0)
+	return (0);
 	}
 }
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file - main.h
+ * Author: jocado49
+ * Desc: Header contains protypes for all functions
+ * used in 0x03-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
